8dd1fc0d35b12e07066ef5511b3797dcc22052e5
Dipti R <razdan.dipti@gmail.com>
2021-06-30 17:20:53 -0400

    Updated codes

---
+++ public/exercise.js
@@ -0,0 +1,242 @@
+const workoutTypeSelect = document.querySelector("#type");
+const cardioForm = document.querySelector(".cardio-form");
+const resistanceForm = document.querySelector(".resistance-form");
+// added the other forms
+const balanceForm = document.querySelector(".balance-form");
+const flexibilityForm = document.querySelector(".flexibility-form");
+// end
+const cardioNameInput = document.querySelector("#cardio-name");
+const nameInput = document.querySelector("#name");
+//names of the Selectors for the added forms
+const balanceNameInput = document.querySelector("#balance-name");
+const flexibilityNameInput = document.querySelector("#flexibility-name");
+// end
+const weightInput = document.querySelector("#weight");
+const setsInput = document.querySelector("#sets");
+const repsInput = document.querySelector("#reps");
+const durationInput = document.querySelector("#duration");
+const resistanceDurationInput = document.querySelector("#resistance-duration");
+const distanceInput = document.querySelector("#distance");
+//inputs for the added forms
+const balancesetsInput = document.querySelector("#balance-sets");
+const balancerepsInput = document.querySelector("#balance-reps");
+const balanceDurationInput = document.querySelector("#balance-duration");
+const flexibilitysetsInput = document.querySelector("#flexibility-sets");
+const flexibilityrepsInput = document.querySelector("#flexibility-reps");
+// end
+const completeButton = document.querySelector("button.complete");
+const addButton = document.querySelector("button.add-another");
+const toast = document.querySelector("#toast");
+const newWorkout = document.querySelector(".new-workout")
+
+
+
+
+let workoutType = null;
+let shouldNavigateAway = false;
+
+async function initExercise() {
+  let workout;
+
+  if (location.search.split("=")[1] === undefined) {
+    workout = await API.createWorkout()
+    console.log(workout)
+  }
+  if (workout) {
+    location.search = "?id=" + workout._id;
+  }
+
+}
+
+initExercise();
+
+function handleWorkoutTypeChange(event) {
+  workoutType = event.target.value;
+  //added the additional conditions
+  if (workoutType === "cardio") {
+    cardioForm.classList.remove("d-none");
+    resistanceForm.classList.add("d-none");
+    balanceForm.classList.add("d-none");
+    flexibilityForm.classList.add("d-none");
+
+  } else if (workoutType === "resistance") {
+    resistanceForm.classList.remove("d-none");
+    cardioForm.classList.add("d-none");
+    balanceForm.classList.add("d-none");
+    flexibilityForm.classList.add("d-none");
+
+  } else if (workoutType === "balance") {
+    resistanceForm.classList.add("d-none");
+    cardioForm.classList.add("d-none");
+    balanceForm.classList.remove("d-none");
+    flexibilityForm.classList.add("d-none");
+
+  }else if (workoutType === "flexibility") {
+    resistanceForm.classList.add("d-none");
+    cardioForm.classList.add("d-none");
+    balanceForm.classList.add("d-none");
+    flexibilityForm.classList.remove("d-none");
+
+  }else {
+    cardioForm.classList.add("d-none");
+    resistanceForm.classList.add("d-none");
+    balanceForm.classList.add("d-none");
+    flexibilityForm.classList.add("d-none");
+  }
+
+  validateInputs();
+}
+
+function validateInputs() {
+  let isValid = true;
+
+  if (workoutType === "resistance") {
+    if (nameInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (weightInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (setsInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (repsInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (resistanceDurationInput.value.trim() === "") {
+      isValid = false;
+    }
+  } else if (workoutType === "cardio") {
+    if (cardioNameInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (durationInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (distanceInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    //Added the additional conditions 
+  } else if (workoutType === "balance") {
+    if (balanceNameInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (balanceDurationInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (balancesetsInput.value.trim() === "") {
+      isValid = false;
+    }
+    if (balancerepsInput.value.trim() === "") {
+      isValid = false;
+    }
+  } else if (workoutType === "flexibility") {
+    if (flexibilityNameInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (balancesetsInput.value.trim() === "") {
+      isValid = false;
+    }
+    if (balancerepsInput.value.trim() === "") {
+      isValid = false;
+    }
+  } 
+
+  if (isValid) {
+    completeButton.removeAttribute("disabled");
+    addButton.removeAttribute("disabled");
+  } else {
+    completeButton.setAttribute("disabled", true);
+    addButton.setAttribute("disabled", true);
+  }
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let workoutData = {};
+
+  if (workoutType === "cardio") {
+    workoutData.type = "cardio";
+    workoutData.name = cardioNameInput.value.trim();
+    workoutData.distance = Number(distanceInput.value.trim());
+    workoutData.duration = Number(durationInput.value.trim());
+  } else if (workoutType === "resistance") {
+    workoutData.type = "resistance";
+    workoutData.name = nameInput.value.trim();
+    workoutData.weight = Number(weightInput.value.trim());
+    workoutData.sets = Number(setsInput.value.trim());
+    workoutData.reps = Number(repsInput.value.trim());
+    workoutData.duration = Number(resistanceDurationInput.value.trim());
+
+    //Added the conditions
+  } else if (workoutType === "balance") {
+    workoutData.type = "balance";
+    workoutData.name = balanceNameInput.value.trim();
+    workoutData.sets = Number(balancesetsInput.value.trim());
+    workoutData.reps = Number(balancerepsInput.value.trim());
+    workoutData.duration = Number(balanceDurationInput.value.trim());
+  } else if (workoutType === "flexibility") {
+    workoutData.type = "flexibility";
+    workoutData.name = flexibilityNameInput.value.trim();
+    workoutData.sets = Number(flexibilitysetsInput.value.trim());
+    workoutData.reps = Number(flexibilityrepsInput.value.trim());
+  }
+
+  await API.addExercise(workoutData);
+  clearInputs();
+  toast.classList.add("success");
+}
+
+function handleToastAnimationEnd() {
+  toast.removeAttribute("class");
+  if (shouldNavigateAway) {
+    location.href = "/";
+  }
+}
+
+function clearInputs() {//Added the details for clearing data
+  cardioNameInput.value = "";
+  nameInput.value = "";
+  balanceNameInput.value = "";
+  flexibilityNameInput.value = "";
+  setsInput.value = "";
+  balancesetsInput.value = "";
+  flexibilitysetsInput.value = "";
+  distanceInput.value = "";
+  durationInput.value = "";
+  repsInput.value = "";
+  balancerepsInput.value = "";
+  flexibilityrepsInput.value = "";
+  resistanceDurationInput.value = "";
+  balanceDurationInput.value = "";
+  weightInput.value = "";
+}
+
+if (workoutTypeSelect) {
+  workoutTypeSelect.addEventListener("change", handleWorkoutTypeChange);
+}
+if (completeButton) {
+  completeButton.addEventListener("click", function (event) {
+    shouldNavigateAway = true;
+    handleFormSubmit(event);
+  });
+}
+if (addButton) {
+  addButton.addEventListener("click", handleFormSubmit);
+}
+toast.addEventListener("animationend", handleToastAnimationEnd);
+
+document
+  .querySelectorAll("input")
+  .forEach(element => element.addEventListener("input", validateInputs));
