820e3fa84bec787aa724f9864403ee8cfd7e30df
Dipti R <razdan.dipti@gmail.com>
2021-07-08 20:15:59 -0400

    deploying the app

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const router = require("./routes/api.js");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+let db = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutDB",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+// routes
+app.use(router);
+require("./routes/html.js");
+require("./routes/api.js");
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = db;
